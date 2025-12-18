8f791d23b980773a18f7c04cbfed24f360599596
stonedch <stch.max.muhin@gmail.com>
2025-12-18 21:06:21 +0300

    feat:initial

---
+++ src/test/java/com/faickoff/api/auth/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faickoff.api.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
